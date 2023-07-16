a7b5ab989a52871cd3c99fd620aba0b302d394f6
dev-lucas <silvaluxas5@gmail.com>
2023-07-16 18:08:18 -0300

    base drawer screen finished

--- lib/screens/home_screen/components/widgets/custom_drawer_widget.dart
+++ lib/screens/home_screen/components/widgets/custom_drawer_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dekor_farben_app/global/constants.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawerWidget extends StatelessWidget {
@@ -5,9 +6,92 @@ class CustomDrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: const Color.fromARGB(255, 108, 142, 203)
+    return Container(
+      color: const Color.fromARGB(255, 7, 14, 37),
+      padding: const EdgeInsets.only(top: 50, bottom: 70, left: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Lucas Silva de Oliveira',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Text(
+                    'Active Status',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  )
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.admin_panel_settings,
+                        color: kDefaultSubtitleColor,
+                        size: 30,
+                      ),
+                      Text(
+                        "Campaigns",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.settings,
+                    color: kDefaultSubtitleColor,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Settings',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.logout,
+                    color: kDefaultSubtitleColor,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Log out',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
